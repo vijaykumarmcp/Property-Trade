e689cffa7d526c63d60668779b376b15cee470be
vijaykumarmcp <43601069+vijaykumarmcp@users.noreply.github.com>
2020-03-14 19:34:40 +0530

    Listing Model added and postgres setup

---
+++ property/property/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('',include('pages.urls')),
+    path('listings',include('listings.urls')),
+    #path('realtors',include('realtors.urls')),
+    path('admin/', admin.site.urls),
+    
+]
---
+++ property_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\dev\repos\property-trade\property_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
